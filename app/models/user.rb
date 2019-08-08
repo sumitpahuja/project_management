98038af78846111c29fca43831b30206cdccdeda
sumitp <sumit@reshotel.in>
2019-08-09 01:28:53 +0530

    find users by roles (by metaprogramming)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   rolify
+  ROLES = %w[admin developer].freeze
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,5 +10,12 @@ class User < ApplicationRecord
 
   # associations
   has_and_belongs_to_many :projects, join_table: 'developers_projects', association_foreign_key: 'project_id', foreign_key: 'developer_id'       
+
+
+  # metaprogramming to find users role wise and their count
+  ROLES.each do |role|
+    scope "#{role.downcase}s", -> { Role.find_by_name(role).users rescue [] }
+    scope "#{role.downcase}_count", -> { Role.find_by_name(role).users.count rescue 0 }
+  end  
 
 end
