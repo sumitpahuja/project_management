db454b04e98e4efa4f1cfb1d8f63f05e4c03907f
sumitp <sumit@reshotel.in>
2019-08-10 18:14:24 +0530

    add project specs

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Project, :type => :model do
-  it "is valid with valid attributes"
+  it "should not create project without name" do
+    project = Project.create(start_date: Date.today + 4.days)
+    expect(project.errors.count).to be > 0
+  end
+
+  it "should not create project without start_date" do
+    project = Project.create(name: 'database optimization')
+    expect(project.errors.count).to be > 0
+  end  
 end
